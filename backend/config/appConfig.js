f6b9e2929f5d3045209b042a8f3ef11a997fcb65
vishal-rajpoot <vishalrajpoot540@gmail.com>
2024-09-05 17:23:12 +0530

    changes

--- backend/config/appConfig.js
+++ backend/config/appConfig.js
@@ -1,22 +1,12 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+
 export default {
   app: {
     port: process.env.PORT || 8088,
-    appName: process.env.APP_NAME || "psm-api",
     env: process.env.NODE_ENV || "dev",
-    mongo_url: process.env.MONGO_URI ,
-  },
-  db: {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
-    database: process.env.DB_NAME || "psm",
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "12345",
-    logging: process.env.DB_LOGGING || false,
-    connPoolLimit: process.env.DB_CONN_POOL_LIMIT || 5,
-    connectTimeout: process.env.DB_CONN_TIME_OUT || 60000,
+    mongo_url: process.env.MONGO_URI,
   },
   winiston: {
     logpath: "./logs/",
